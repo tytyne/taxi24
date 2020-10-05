fe5871ba8a19a9f4e506cc5760c62c254a697d37
tytyne <dusaflora2@gmail.com>
2020-10-05 21:47:33 +0200

    (feature(adding testing) testing [finish(test)]

---
+++ test/routes.test.js
@@ -0,0 +1,22 @@
+// let chai=require("chai")
+// let expect =require("chai").expect;
+// let chaiHttp=require("chai-http")
+// let server=require("../index")
+// let should=require("chai").should();
+// chai.use(chaiHttp)
+
+// describe('all drivers',()=>{
+
+//     it('it should get all the drivers',(done)=>{
+//         chai.request(server)
+//         .get('/api/alldrivers')
+//         .end((req,res)=>{
+
+//             res.should.have.status(200);
+//             res.body.should.be.a('array')
+//             res.body.length.should.be.eql(0);
+//             done();
+//         });
+        
+//     });
+// });
