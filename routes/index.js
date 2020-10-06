3f235a0884174528923c62f356bf549e50bb542c
tytyne <dusaflora2@gmail.com>
2020-10-07 00:19:28 +0200

    change routes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const riderControllers = require('../controllers/getRiders')
 const tripControllers=require('../controllers/tripcontr')
 const router=express.Router()
 
-router.get('/alldrivers',driverControllers.allDrivers); //all drivers
-router.get('/available/drivers',driverControllers.driverByStatus);//available drivers 
-router.get('/available/range/drivers',driverControllers.all); //3km away
-router.get('/driver/:id',driverControllers.driverById);// by id
+router.get('/alldrivers',driverControllers.allDrivers); //all drivers //completed
+router.get('/available/drivers',driverControllers.driverByStatus);//available drivers // completed
+router.get('/available/range/drivers',driverControllers.all); //3km away //completed
+router.get('/driver/:id',driverControllers.driverById);// by id //completed
 
 
 
@@ -17,8 +17,8 @@ router.get('/complete/trip/:id',tripControllers.completeTrip) //complete a trip
 router.get('/active/trips',tripControllers.tripByStatus) //active trips
 
 
-router.get('/allriders',riderControllers.allRiders); // all riders
-router.get('/rider/:id',riderControllers.riderById); // rider by ID
-router.get('/closer/rider/:id',riderControllers.closeToriders) // 3 close drivers 
+router.get('/allriders',riderControllers.allRiders); // all riders //completed 
+router.get('/rider/:id',riderControllers.riderById); // rider by ID //completed
+router.get('/closer/rider/:id',riderControllers.closeToriders) // 3 close drivers //completed
 
 module.exports=router
