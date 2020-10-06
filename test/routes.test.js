a11847b00614475ed45a91f52e10ff529fea108c
tytyne <dusaflora2@gmail.com>
2020-10-06 13:27:30 +0200

    change link

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,22 +1,22 @@
-// let chai=require("chai")
-// let expect =require("chai").expect;
-// let chaiHttp=require("chai-http")
-// let server=require("../index")
-// let should=require("chai").should();
-// chai.use(chaiHttp)
+let chai=require("chai")
+let expect =require("chai").expect;
+let chaiHttp=require("chai-http")
+let server=require("../index")
+let should=require("chai").should();
+chai.use(chaiHttp)
 
-// describe('all drivers',()=>{
+describe('all drivers',()=>{
 
-//     it('it should get all the drivers',(done)=>{
-//         chai.request(server)
-//         .get('/api/alldrivers')
-//         .end((req,res)=>{
+    it('it should get all the drivers',(done)=>{
+        chai.request(server)
+        .get('/api/alldrivers')
+        .end((req,res)=>{
 
-//             res.should.have.status(200);
-//             res.body.should.be.a('array')
-//             res.body.length.should.be.eql(0);
-//             done();
-//         });
+            res.should.have.status(200);
+            res.body.should.be.a('array')
+            res.body.length.should.be.eql(0);
+            done();
+        });
         
-//     });
-// });
+    });
+});
